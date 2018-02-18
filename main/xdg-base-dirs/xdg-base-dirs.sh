565ed22de253e1a43045a9979436f96404843ab7
Ain <41307858+nero@users.noreply.github.com>
2018-02-18 13:17:35 +0000

    xdg-base-dirs: Dont force /etc/user if ~/.config exists

--- main/xdg-base-dirs/xdg-base-dirs.sh
+++ main/xdg-base-dirs/xdg-base-dirs.sh
@@ -1,4 +1,14 @@
 #!/bin/sh
-[ -w "/etc/user/$(id -u)" ]       && export XDG_CONFIG_HOME="/etc/user/$(id -u)"
-[ -w "/var/cache/user/$(id -u)" ] && export XDG_CACHE_HOME="/var/cache/user/$(id -u)"
-[ -w "/run/user/$(id -u)" ]       && export XDG_RUNTIME_DIR="/run/user/$(id -u)"
+if [ -e "$HOME/.config" ]; then
+  export XDG_CONFIG_HOME="$(readlink -f "$HOME/.config")"
+elif [ -w "/etc/user/$(id -u)" ]; then
+  export XDG_CONFIG_HOME="/etc/user/$(id -u)"
+fi
+
+if [ -w "/var/cache/user/$(id -u)" ]; then
+  export XDG_CACHE_HOME="/var/cache/user/$(id -u)"
+fi
+
+if [ -w "/run/user/$(id -u)" ]; then
+  export XDG_RUNTIME_DIR="/run/user/$(id -u)"
+fi
