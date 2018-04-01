3682b5fe622342272537a3c39e8a4f8c5c7602ea
Ain <41307858+nero@users.noreply.github.com>
2018-04-01 18:18:37 +0000

    xdg-base-dirs: Dont set XDG_CONFIG_HOME
    User profile should do this.

--- main/xdg-base-dirs/xdg-base-dirs.sh
+++ main/xdg-base-dirs/xdg-base-dirs.sh
@@ -1,9 +1,4 @@
 #!/bin/sh
-if [ -e "$HOME/.config" ]; then
-  export XDG_CONFIG_HOME="$(readlink -f "$HOME/.config")"
-elif [ -w "/etc/user/$(id -u)" ]; then
-  export XDG_CONFIG_HOME="/etc/user/$(id -u)"
-fi
 
 if [ -w "/var/cache/user/$(id -u)" ]; then
   export XDG_CACHE_HOME="/var/cache/user/$(id -u)"
